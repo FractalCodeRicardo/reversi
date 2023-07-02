08efbe5b6fe04c8f7623b8eb450b1e3e7a3e8120
Ricardo Serrato B <rsbse@hotmail.com>
2023-07-02 16:54:18 -0600

    Changes

--- Drawer.cpp
+++ Drawer.cpp
@@ -97,7 +97,7 @@ void Drawer::setColor(int color)
 
 void Drawer::text(int x, int y, int size, char* texto)
 {
-    TTF_Font *Sans = TTF_OpenFont("Sans.ttf", size);
+    /*TTF_Font *Sans = TTF_OpenFont("Sans.ttf", size);
     SDL_Color White = {255, 255, 255};
     SDL_Surface *surfaceMessage =
         TTF_RenderText_Solid(Sans, texto, White);
@@ -113,5 +113,5 @@ void Drawer::text(int x, int y, int size, char* texto)
     SDL_RenderCopy(renderer, Message, NULL, &Message_rect);
 
     SDL_FreeSurface(surfaceMessage);
-    SDL_DestroyTexture(Message);
+    SDL_DestroyTexture(Message);*/
 }
